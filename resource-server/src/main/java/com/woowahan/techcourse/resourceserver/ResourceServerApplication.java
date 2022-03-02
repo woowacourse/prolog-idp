ce95e84a981c9e006e5cb80f344a73782cc07bc2
Eugine Park <pkeugine@gmail.com>
2022-03-02 18:40:02 +0900

    etc: include resource server

---
+++ resource-server/src/main/java/com/woowahan/techcourse/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.woowahan.techcourse.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+}
---
+++ resource-server/src/test/java/com/woowahan/techcourse/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowahan.techcourse.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
