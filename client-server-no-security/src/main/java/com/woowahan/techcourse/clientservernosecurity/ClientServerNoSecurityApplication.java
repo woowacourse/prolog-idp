18d0918be9527f900d4fe879f5b526febce3b292
Eugine Park <pkeugine@gmail.com>
2022-07-26 02:41:55 +0900

    etc: create client server with no security

---
+++ client-server-no-security/src/main/java/com/woowahan/techcourse/clientservernosecurity/ClientServerNoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.woowahan.techcourse.clientservernosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerNoSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerNoSecurityApplication.class, args);
+    }
+
+}
---
+++ client-server-no-security/src/test/java/com/woowahan/techcourse/clientservernosecurity/ClientServerNoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowahan.techcourse.clientservernosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerNoSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
