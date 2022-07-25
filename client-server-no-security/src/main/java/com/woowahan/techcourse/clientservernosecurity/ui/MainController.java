6d958696195016fac194f4a12072bb5fadf1bbdf
Eugine Park <pkeugine@gmail.com>
2022-07-26 02:48:02 +0900

    feat: create simple main page for no security client

---
+++ client-server-no-security/src/main/java/com/woowahan/techcourse/clientservernosecurity/ui/MainController.java
@@ -0,0 +1,11 @@
+package com.woowahan.techcourse.clientservernosecurity.ui;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    public String index() {
+        return "index";
+    }
+}
